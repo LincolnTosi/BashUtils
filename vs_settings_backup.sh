78182943a5b9ca04c03440cc44f0e374409ec6cf
Lincoln Tosi <lincoln.tosi@gmail.com>
2015-09-26 16:08:20 -0700

    Update vs_settings_backup.sh

--- vs_settings_backup.sh
+++ vs_settings_backup.sh
@@ -1,5 +1,5 @@
 # Lincoln Tosi 2/20/14
-# automatically copies Visual Assist and AStyle settings to Google Drive
+# automates exportation of Visual Assist and AStyle settings to Google Drive
 
 script_description="UPATE VISUALSTUDIO 2013 SETTINGS"
 GDDIR="D:\Google Drive\Settings\Visual Studio 2013"
